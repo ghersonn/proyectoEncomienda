3c8f9d77fc7f1e9669e2ccc9e4b46135a7387a2e
izquierdoMelgar <ghersonn_1995@hotmail.com>
2016-11-15 01:48:15 -0500

    Conexion BD

--- CapaDatos/src/com/dao/DAOConexion.java
+++ CapaDatos/src/com/dao/DAOConexion.java
@@ -1,5 +1,30 @@
 package com.dao;
 
-public class DAOConexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAOConexion {
+	//Singleton
+	public static DAOConexion _Instancia;
+	private DAOConexion(){};
+	public static DAOConexion Instancia(){
+		if(_Instancia==null){
+			_Instancia = new DAOConexion();
+		}
+		return _Instancia;
+	}
+	//endSingleton
+	
+	public Connection conectar() throws Exception{
+		Connection cn = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			cn = DriverManager.getConnection("jdbc:sqlserver://104.197.224.95:1433;"+
+						"databaseName=SOWAD_Encomienda", "sa", "P@ssw0rd12");
+		} catch (Exception e) {
+			throw e;
+		}
+		return cn;
+	}
 }
+
